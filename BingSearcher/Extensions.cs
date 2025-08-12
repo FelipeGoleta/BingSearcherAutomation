6e4f2b82548344d322da499fb578222277c8b935
Felipe Goleta <frgoleta@gmail.com>
2025-08-12 12:38:21 +0800

    Added an extension class for simulating random delays

---
+++ BingSearcher/Extensions.cs
@@ -0,0 +1,11 @@
+﻿namespace BingSearcher;
+
+public static class Extensions
+{
+    private static readonly Random _random = new Random();
+    public static async Task RandomDelay(this Task task, int minDelay = 1000, int maxDelay = 2000)
+    {
+        var delay = _random.Next(minDelay, maxDelay);
+        await Task.Delay(delay);
+    }
+}
