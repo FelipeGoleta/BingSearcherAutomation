c46a27071b6f45809aacb0f5866f992a27b43418
Felipe Goleta <frgoleta@gmail.com>
2025-08-12 12:39:48 +0800

    New Feature: Searches the bonus points

--- BingSearcher/Program.cs
+++ BingSearcher/Program.cs
@@ -2,27 +2,68 @@
 using OpenQA.Selenium;
 using CrypticWizard.RandomWordGenerator;
 
-const int _wordCount = 90;
+namespace BingSearcher;
 
-var options = new EdgeOptions();
-var driverPath = @"C:\Users\Oyiebhoy\Downloads\msedgedriver.exe";
-var service = EdgeDriverService.CreateDefaultService(driverPath);
-
-using (var driver = new EdgeDriver(service, options))
+public static class Program
 {
-    var wordGenerator = new WordGenerator();
-    var words = wordGenerator.GetWords(_wordCount);
-    foreach (var stringQuery in words)  
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine($"Searching for: {stringQuery}");
-        driver.Navigate().GoToUrl("https://www.bing.com");
-        await Task.Delay(1000);
+        const int _wordCount = 90;
 
-        var searchBox = driver.FindElement(By.Name("q"));
-        searchBox.Clear();
-        searchBox.SendKeys(stringQuery);
-        searchBox.SendKeys(Keys.Enter);
-        await Task.Delay(1000);
+        var options = new EdgeOptions();
+        var driverPath = @"C:\Users\Oyiebhoy\Downloads\msedgedriver.exe";
+        var service = EdgeDriverService.CreateDefaultService(driverPath);
+        var random = new Random();
+
+        using (var driver = new EdgeDriver(service, options))
+        {
+            var wordGenerator = new WordGenerator();
+            var words = wordGenerator.GetWords(_wordCount);
+            foreach (var stringQuery in words)
+            {
+                Console.WriteLine($"Searching for: {stringQuery}");
+                driver.Navigate().GoToUrl("https://www.bing.com");
+                await Task.CompletedTask.RandomDelay();
+
+                var searchBox = driver.FindElement(By.Name("q"));
+                searchBox.Clear();
+                searchBox.SendKeys(stringQuery);
+                searchBox.SendKeys(Keys.Enter);
+                await Task.CompletedTask.RandomDelay();
+            }
+
+            driver.Navigate().GoToUrl("https://rewards.bing.com/?form=ML2UYJ");
+            await Task.Delay(1000);
+
+            var anchorElements = driver.FindElements(By.TagName("a"));
+
+            foreach (var anchorElement in anchorElements)
+            {
+                try
+                {
+                    var href = anchorElement.GetAttribute("href");
+                    if (href is null || !href.Contains("https://www.bing.com/search?q="))
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"Found link: {href}");
+                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", anchorElement);
+                    await Task.CompletedTask.RandomDelay();
+
+                    anchorElement.Click();
+                    await Task.CompletedTask.RandomDelay();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            driver.Close();
+        }
     }
 }
+
 
